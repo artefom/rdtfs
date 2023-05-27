bf675e99eeea90033bd80c438a084aae93c68729
Artyom Fomenko <artyom.fomenko@distribusion.com>
2023-05-27 14:00:39 +0200

    WIP

--- src/binarystore.rs
+++ src/binarystore.rs
@@ -11,73 +11,11 @@ use anyhow::{Context, Result};
 use serde::{de::DeserializeOwned, Serialize};
 use tempfile::{tempdir, TempDir};
 
-struct BinaryWriter {
-    path: PathBuf,
-    buf: Vec<u8>,
-}
+use binarystore::BinaryWriter;
 
-impl BinaryWriter {
-    fn new(path: PathBuf) -> Self {
-        BinaryWriter {
-            path,
-            buf: Vec::new(),
-        }
-    }
+use self::binarystore::BinaryReader;
 
-    fn into_path(self) -> Result<PathBuf> {
-        Ok(self.path)
-    }
-
-    pub fn write_one<S: Serialize>(&mut self, data: &S) -> Result<()> {
-        bincode::serialize_into(&mut self.buf, data).context("Could not serialize data")?;
-        if self.buf.len() > 8000 {
-            self.flush()?;
-        }
-        Ok(())
-    }
-
-    pub fn flush(&mut self) -> Result<()> {
-        if self.buf.len() == 0 {
-            return Ok(());
-        };
-        let mut file = OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(&self.path)?;
-        file.write(&self.buf)?;
-        self.buf.clear();
-        Ok(())
-    }
-}
-
-struct BinaryReader<R: Read> {
-    reader: R,
-}
-
-impl<R: Read> BinaryReader<R> {
-    fn new(reader: R) -> Self {
-        BinaryReader { reader }
-    }
-}
-
-impl<R: Read> BinaryReader<R> {
-    pub fn read_one<D: DeserializeOwned>(&mut self) -> Result<Option<D>> {
-        // let num_bytes = bincode::deserialize_from(&mut self.reader)?;
-
-        let object = match bincode::deserialize_from(&mut self.reader) {
-            Ok(value) => Ok(value),
-            Err(err) => {
-                if let bincode::ErrorKind::Io(_) = *err {
-                    return Ok(None);
-                } else {
-                    Err(err)
-                }
-            }
-        }?;
-
-        Ok(Some(object))
-    }
-}
+mod binarystore;
 
 pub struct PartitionedStoreWriter<S, H, K>
 where
---
+++ src/binarystore/binarystore.rs
@@ -0,0 +1,80 @@
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap, HashSet},
+    fs::{File, OpenOptions},
+    hash::{Hash, Hasher},
+    io::{BufReader, Read, Write},
+    marker::PhantomData,
+    path::PathBuf,
+};
+
+use anyhow::{Context, Result};
+use serde::{de::DeserializeOwned, Serialize};
+use tempfile::{tempdir, TempDir};
+
+pub struct BinaryWriter {
+    path: PathBuf,
+    buf: Vec<u8>,
+}
+
+impl BinaryWriter {
+    pub fn new(path: PathBuf) -> Self {
+        BinaryWriter {
+            path,
+            buf: Vec::new(),
+        }
+    }
+
+    pub fn into_path(self) -> Result<PathBuf> {
+        Ok(self.path)
+    }
+
+    pub fn write_one<S: Serialize>(&mut self, data: &S) -> Result<()> {
+        bincode::serialize_into(&mut self.buf, data).context("Could not serialize data")?;
+        if self.buf.len() > 8000 {
+            self.flush()?;
+        }
+        Ok(())
+    }
+
+    pub fn flush(&mut self) -> Result<()> {
+        if self.buf.len() == 0 {
+            return Ok(());
+        };
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(&self.path)?;
+        file.write(&self.buf)?;
+        self.buf.clear();
+        Ok(())
+    }
+}
+
+pub struct BinaryReader<R: Read> {
+    reader: R,
+}
+
+impl<R: Read> BinaryReader<R> {
+    pub fn new(reader: R) -> Self {
+        BinaryReader { reader }
+    }
+}
+
+impl<R: Read> BinaryReader<R> {
+    pub fn read_one<D: DeserializeOwned>(&mut self) -> Result<Option<D>> {
+        // let num_bytes = bincode::deserialize_from(&mut self.reader)?;
+
+        let object = match bincode::deserialize_from(&mut self.reader) {
+            Ok(value) => Ok(value),
+            Err(err) => {
+                if let bincode::ErrorKind::Io(_) = *err {
+                    return Ok(None);
+                } else {
+                    Err(err)
+                }
+            }
+        }?;
+
+        Ok(Some(object))
+    }
+}
